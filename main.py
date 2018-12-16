2ff66eb988b1e24d1526aee6cf358910a5820042
Abby Beizer <abbybeizer@gmail.com>
2018-12-16 15:29:21 -0500

    Test the update probabilities function

--- main.py
+++ main.py
@@ -10,17 +10,25 @@ def main():
     grid = Grid()   # EV3's internal representation of the colored grid.
 
     # Confirm that we can print out the color of each square in the grid
-    print_init_grid_probs(grid)
+    #print_init_grid_probs(grid)
 
-    print(grid.determine_starting_position(Color.GREEN, Color.RED))
-    print(grid.squares[0][1].color, " --> ", grid.squares[0][1].probability)
+    
+    # Test update_probabilities
+    # Pretend we are moving from square(0,1) to square(0,2)
 
+    x, y = grid.determine_starting_position(Color.GREEN, Color.RED)
+    print("[", x, ",", y, "]")
+    print(grid.squares[x][y].color, " --> ", grid.squares[x][y].probability)
+    print()
+    x, y = grid.update_probabilities(Color.GREEN, Color.RED, Color.BLUE, Color.RED)
+    print("[", x, ",", y, "]")
+    print(grid.squares[x][y].color, " --> ", grid.squares[x][y].probability)
+    print()
     # Examples from various classes
     # drive_example()
 
 
 def print_init_grid_probs(myGrid):
-    # Confirm that we can print out the color of each square in the grid
     for row in myGrid.squares:
         for s in row:
             print(s.color, " --> ", s.horizontal_neighbors[0].color, " ", s.horizontal_neighbors[1].color,
@@ -28,14 +36,15 @@ def print_init_grid_probs(myGrid):
             print(s.horizontal_neighbors[0].probability)
     print()
 
+    
 
-def drive_example():
+
+#def drive_example():
     # Show basic capabilities from drive.py, some unfinished
-    turn_left()
-    turn_right()
-    move_straight()
-    stop()
-
+    #turn_left()
+    #turn_right()
+    #move_straight()
+    #stop()
 
 if __name__ == '__main__':
     main()
