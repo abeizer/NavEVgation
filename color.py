890f8f30e3fa79101c392f63ad7a2544628ffdf8
Abby Beizer <abbybeizer@gmail.com>
2018-12-17 23:18:57 -0500

    Update color.py

--- color.py
+++ color.py
@@ -2,14 +2,14 @@
 This Color enum is used for consistency throughout the rest of the program.
 Any values used to represent the colors will all agree with this enum.
 
-Author: Abby Beizer, Joe Hammer
+Author: Abby Beizer
 '''
 
 from enum import Enum
 
 
 class Color(Enum):
-    NO COLOR = 0
+    NO_COLOR = 0
     BLACK = 1
     BLUE = 2
     GREEN = 3
